3b0f898d195f234c7b9b550e398e20a75b5ef5f0
zhanpan <1045361304@qq.com>
2018-07-19 17:42:10 +0800

    启动图显示正常

--- android/app/src/main/java/com/awesomeproject/MainActivity.java
+++ android/app/src/main/java/com/awesomeproject/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends ReactActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // SplashScreen.show(this);   //add
+        SplashScreen.show(this);   //add
         super.onCreate(savedInstanceState);
     }
 
